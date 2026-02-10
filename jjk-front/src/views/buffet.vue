7aee374ecd5a07669e3709463acbccb6bc2f3053
Rebecca Bracho <rebeccabracho05@gmail.com>
2026-02-10 13:09:12 -0400

    feat: change link

--- jjk-front/src/views/buffet.vue
+++ jjk-front/src/views/buffet.vue
@@ -11,11 +11,11 @@ import Button from 'primevue/button';
   <h2 class="text-lg font-normal text-body lg:text-xl">Upload the case file to send to the prosecutor office</h2>
   <div class="p-4 m-3">
   <div class="p-4 m-3 flex items-center justify-center gap-3">
-    <FileUpload mode="basic" name="casefile" url="http://localhost:8080/upload" accept=".pdf" maxFileSize="1000000" class="inline-block" />
-    <Button label="Submit Case" />
-  </div>
-  </div>
+    <FileUpload mode="basic" name="casefile" url="/upload" accept=".pdf" :maxFileSize="1000000" class="inline-block" :auto="true"/>
+    
+            </div>
     </div>
+  </div>
 </template>
 
 <style scoped></style>
